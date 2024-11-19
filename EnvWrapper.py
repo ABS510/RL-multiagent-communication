033b67255271ca127d4464c54d6b95f7bab2b66e
Yining Hong <yhong3@andrew.cmu.edu>
2024-11-19 11:44:45 -0500

    finish wrapper

--- EnvWrapper.py
+++ EnvWrapper.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import List, Tuple, Dict
 import functools
 
 import numpy as np
 import gymnasium as gym
-from pettingzoo.utils.wrappers import OrderEnforcingWrapper
+from pettingzoo.utils.conversions import aec_to_parallel_wrapper
 
 from Logging import setup_logger
 
@@ -37,6 +37,19 @@ class Intention:
         self._candidates = candidates
         self._default = default
         self._val = default
+
+    def check_validity(self, agents: List[str]):
+        """Checks if the source and destination agents are valid.
+
+        Args:
+            agents (List[str]): The list of agents in the environment.
+        """
+
+        if self._src_agent not in agents:
+            raise ValueError("Invalid source agent")
+        for agent in self._dst_agents:
+            if agent not in agents:
+                raise ValueError("Invalid destination agent")
 
     def __repr__(self) -> str:
         return (
@@ -86,7 +99,9 @@ class Intention:
         Returns:
             gym.spaces.Box: A gym.spaces.Box space with a shape corresponding to the number of candidates.
         """
-        return gym.spaces.Box(low=0, high=1, shape=(len(self._candidates),), dtype=np.uint8)
+        return gym.spaces.Box(
+            low=0, high=1, shape=(len(self._candidates),), dtype=np.uint8
+        )
 
     def reset(self) -> None:
         """
@@ -94,14 +109,38 @@ class Intention:
         """
         self._val = self._default
 
+    def observe(self) -> np.ndarray:
+        """
+        Returns the one-hot vector representing the current intention.
 
-class EnvWrapper(OrderEnforcingWrapper):
-    def __init__(self, base_env: OrderEnforcingWrapper):
+        Returns:
+            np.ndarray: A one-hot vector representing the current intention.
+        """
+        obs = np.zeros(len(self._candidates))
+        obs[self._val] = 1
+        return obs
+
+    def step(self, action: np.ndarray):
+        """
+        Updates the intention based on the given action.
+
+        Args:
+            action (np.ndarray): The action to take.
+        """
+        action = int(action)
+        if not (0 <= action < len(self._candidates)):
+            raise ValueError("Invalid action index")
+        self._val = action
+
+
+class EnvWrapper(aec_to_parallel_wrapper):
+    def __init__(self, base_env: aec_to_parallel_wrapper):
         logger.info(f"Initializing EnvWrapper with base_env: {base_env}")
-        super().__init__(base_env)
+        super().__init__(base_env.aec_env)
         self._intentions: List[Intention] = []
 
     def add_intention(self, intention: Intention):
+        intention.check_validity(self.agents)
         self._intentions.append(intention)
         logger.info(f"Added intention: {intention}")
 
@@ -115,63 +154,92 @@ class EnvWrapper(OrderEnforcingWrapper):
         return gym.spaces.Tuple(space_list)
 
     @functools.lru_cache(maxsize=None)
-    def action_space(self, agent: str) -> gym.spaces.MultiDiscrete:
-        origin_space = super().action_space(agent).n
-        space_list = [origin_space]
+    def action_space(self, agent: str) -> gym.spaces.Tuple:
+        space_list = [super().action_space(agent)]
         for intention in self._intentions:
             if intention.is_src_agent(agent):
-                space_list.append(intention.action_space.n)
-        return gym.spaces.MultiDiscrete(space_list)
+                space_list.append(intention.action_space)
+        return gym.spaces.Tuple(space_list)
 
-    def render(self):
-        # TODO
-        pass
+    def observe(self, agent: str) -> List[np.ndarray]:
+        observation_list = [self.aec_env.observe(agent)]
+        for intention in self._intentions:
+            if intention.is_dst_agent(agent):
+                observation_list.append(intention.observe())
+        return observation_list
 
-    def observe(self, agent: str):
-        # TODO
-        return super().observe(agent)
-        pass
-
-    def reset(self, seed=None, options=None):
-        super().reset(seed, options)
+    def reset(
+        self, seed=None, options=None
+    ) -> Tuple[Dict[str, List[np.ndarray]], Dict]:
+        _, info = super().reset(seed, options)
         for intention in self._intentions:
             intention.reset()
         logger.info(f"Reset {self}")
+        observations = {agent: self.observe(agent) for agent in self.agents}
+        return observations, info
 
-    def step(self, action):
-        # TODO
-        pass
+    def step(self, action: Dict[str, Tuple[np.ndarray]]):
+        original_action = {agent: action[agent][0] for agent in self.agents}
+        _, rewards, terminations, truncations, infos = super().step(original_action)
+        for agent in self.agents:
+            action_idx = 1
+            for intention in self._intentions:
+                if intention.is_src_agent(agent):
+                    intention.step(action[agent][action_idx])
+                    action_idx += 1
+        observations = {agent: self.observe(agent) for agent in self.agents}
+        rewards = self.add_reward(action, rewards)
+        return observations, rewards, terminations, truncations, infos
 
+    def add_reward(
+        self, action: Dict[str, Tuple[np.ndarray]], rewards: Dict[str, float]
+    ):
+        return rewards
+
+
+# TODO: For customized rewards that depend on the intentions, override the add_reward method.
 
 # Example usage
 if __name__ == "__main__":
     from pettingzoo.atari import volleyball_pong_v3
 
-    base_env = volleyball_pong_v3.env()
+    base_env = volleyball_pong_v3.parallel_env()
     env = EnvWrapper(base_env)
-    env.add_intention(
-        Intention(
-            "first_0",
-            ["first_0", "second_0"],
-            ["no_preference", "stay", "jump"],
-        )
-    )
-    env.add_intention(
-        Intention(
-            "second_0",
-            ["first_0", "second_0"],
-            ["no_preference", "stay", "jump"],
-        )
-    )
     env.reset()
+    agents = env.agents
     logger.info(f"Agents: {env.agents}")
-    logger.info(f"Agent 0 observation space: {env.observation_space(env.agents[0])}")
-    logger.info(f"Agent 0 action space: {env.action_space(env.agents[0])}")
+    env.add_intention(
+        Intention(agents[0], agents[0:2], ["no_preference", "stay", "jump"])
+    )
+    env.add_intention(
+        Intention(agents[1], agents[0:2], ["no_preference", "stay", "jump"])
+    )
+    observations, info = env.reset()
+    logger.info(f"Agents: {agents}")
+    for agent in agents:
+        logger.info(f"Agent {agent} observation space: {env.observation_space(agent)}")
+        logger.info(f"Agent {agent} action space: {env.action_space(agent)}")
+        logger.info(f"Agent {agent} random action: {env.action_space(agent).sample()}")
+        logger.info(
+            f"Agent {agent} random action type: {type(env.action_space(agent).sample())}"
+        )
+        for observation in observations[agent]:
+            logger.info(f"Agent {agent} observation shape: {observation.shape}")
 
-    logger.info(f"Agent_0 agent space type: {type(env.observe(env.agents[0]))}")
-    logger.info(f"Agent_0 agent space shape: {env.observe(env.agents[0]).shape}")
+    frame_num = 1
+    for i in range(frame_num):
+        # insert policy here; use the dictionary observation to get the observation for each agent
+        actions = {
+            agent: env.action_space(agent).sample() for agent in agents
+        }  # random actions
+        observations, rewards, terminations, truncations, infos = env.step(actions)
+        # train network here; use the dictionary observation to get the observation for each agent
 
-    base_env.reset()
-    act_space = base_env.action_space("first_0").n
-    print(act_space)
-    print(np.random.choice(act_space))
+        logger.info("-" * 20)
+        logger.info(f"Frame {i}")
+        for observation in observations[agents[0]]:
+            logger.info(f"Agent {agents[0]} observation shape: {observation.shape}")
+        logger.info(f"Rewards: {rewards}")
+        logger.info(f"Terminations: {terminations}")
+        logger.info(f"Truncations: {truncations}")
+        logger.info(f"Infos: {infos}")
